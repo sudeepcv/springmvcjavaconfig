da7958c96ec8c1d220a2e28e2bae463b61a6ae8f
sudeepcv <basicsinjava@gmail.com>
2019-02-22 12:24:23 +0530

    mvc configuration

---
+++ src/main/basicsinjava/HelloController.java
@@ -0,0 +1,17 @@
+package basicsinjava;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+
+public class HelloController {
+    @RequestMapping("/hello")
+    public  String hello(){
+        System.out.println("inside hello controller");
+        return "index.jsp";
+    }
+
+
+
+}
